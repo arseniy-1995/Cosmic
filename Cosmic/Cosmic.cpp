593e6aaa185657e8d86805d75cae04fb1993b829
arseniy-1995 <arseniy-1995@mail.ru>
2021-03-18 00:35:07 +0700

    1

--- Cosmic/Cosmic.cpp
+++ Cosmic/Cosmic.cpp
@@ -13,7 +13,7 @@ int main(int argc, char** argv)
     TApplication theApp("App", &argc, argv);
 
 
-    TF1* f1 = new TF1("f1", "sin(x)/x", 0, 1);
+    TF1* f1 = new TF1("f1", "sin(x)/x", 0, 1.);
 
     theApp.Run();
 
